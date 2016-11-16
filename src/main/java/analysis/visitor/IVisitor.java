06d76d9c99452879809582ab12f5a90ca6fae00f
Alexandre <tha1n@users.noreply.github.com>
2016-11-16 19:18:47 +0100

    Adding Visitor Interface and Method Visitor. Work has to be done

---
+++ src/main/java/analysis/visitor/IVisitor.java
@@ -0,0 +1,12 @@
+package analysis.visitor;
+
+import spoon.reflect.declaration.CtElement;
+
+/**
+ * TODO --> Adding Comment
+ * Created by alxqu on 16/11/2016.
+ */
+public interface IVisitor {
+
+    public void visit (CtElement ctElement);
+}
