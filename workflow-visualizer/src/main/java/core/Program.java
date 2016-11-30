9cdeedc5ff2153cf79a3bc2bcbd4d8d0ca34d2e0
Steven Pescheteau <steven.pescheteau@gmail.com>
2016-11-30 11:47:29 +0100

    Doc done : alls functions are commented on the interfaces.

---
+++ workflow-visualizer/src/main/java/core/Program.java
@@ -0,0 +1,33 @@
+package core;
+
+import java.util.List;
+
+/**
+ * Created by steve on 17/11/2016.
+ */
+public interface Program {
+
+    /**
+     * Nom du programme
+     * @return le nom du programme
+     */
+    public String getName();
+
+    /**
+     * Regroupe l'ensemble des appels de fonction (typiquement, la fonction main en fait partie)
+     * @return Liste de fonctions appelé à la racine du programme
+     */
+    public List<Function> getCalledFunctions();
+
+    /**
+     * Retourne la fonction actuelle dans lequel l'exécution se fait.
+     * @return Function, si on tourne dans une fonction, null le cas échéant
+     */
+    public Function getCurrentFunction();
+
+    /**
+     * Modifie la fonction actuelle. Ne doit pas être appelée par autre chose qu'une classe du core.
+     * @param function Nouvelle fonction actuelle
+     */
+    public void setCurrentFunction(Function function);
+}
